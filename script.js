ad39cc8d388c3992d3746ad68fa1ace91424a8ee
Samyak Gondane <90952398+SamyakGondane@users.noreply.github.com>
2026-06-05 16:40:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,172 +17,6 @@
 //     return SongList
 // }
 
-// function SecToMin(time) {
-//     if (isNaN(time)) {
-//         return "00:00"
-//     }
-//     let min = Math.floor(time / 60)
-//     let sec = Math.floor(time % 60)
-
-//     if (sec < 10) {
-//         return `${min}:${"0" + sec}`
-//     }
-//     return `${min}:${sec}`
-// }
-
-// function playMusic(element, index) {
-//     audio.src = element
-//     audio.addEventListener("loadedmetadata", () => {
-//         document.querySelector(".songtime").innerText =
-//             `${SecToMin(audio.currentTime)} / ${SecToMin(audio.duration)}`
-//     }, { once: true })
-//     audio.play()
-//     play.src = "/Spotify-Clone/controls/pause.svg"
-//     currsongindex = index
-
-//     let song = element
-//         .replaceAll("http://127.0.0.1:3000/Spotify-Clone/Songs/", "")
-//         .replaceAll("%20", " ")
-//         .replace(".mp3", "")
-
-//     document.querySelector(".songinfo").innerText = song
-
-// }
-
-// let audio = new Audio()
-// let currsongindex = 0
-// let allsongs = []
-
-// let main = async () => {
-//     let songs = await GetSongs()
-//     allsongs = songs
-
-//     let prev = document.querySelector("#prev")
-//     let play = document.querySelector("#play")
-//     let next = document.querySelector("#next")
-
-//     for (let i = 0; i < songs.length; i++) {
-//         let element = songs[i];
-//         let song = element.replaceAll("http://127.0.0.1:3000/Spotify-Clone/Songs/", "").replaceAll("%20", " ").replace(".mp3", "")
-//         let artist = song.split("-")[1]
-
-//         let card = document.querySelector(".card_container")
-//         let div = document.createElement("div")
-//         div.className = "card"
-//         div.innerHTML = `   <img src="https://i.scdn.co/image/ab67706f000000029e1828b742d18f4756bb6c29" alt="">
-//         <h3>${song}</h3>
-//         <p>${artist}</p>`
-//         card.appendChild(div)
-
-
-//         div.addEventListener("click", () => {
-//             playMusic(element, i)
-//         })
-
-//         let middle_card = document.querySelector(".middle")
-//         let middle_div = document.createElement("div")
-//         middle_div.className = "sub_box"
-//         middle_div.innerHTML = `<div><h5>${song}</h5>
-//         <p>${artist}</p></div>
-//         <img src="play_circle.svg" alt="">`
-//         middle_card.appendChild(middle_div)
-
-//         middle_div.addEventListener("click", () => {
-//             playMusic(element, i)
-//         })
-//     }
-
-//     play.addEventListener("click", () => {
-//         if (audio.paused) {
-//             audio.play()
-//             play.src = "/Spotify-Clone/controls/pause.svg"
-//         }
-//         else {
-//             audio.pause()
-//             play.src = "/Spotify-Clone/controls/play.svg"
-//         }
-//     })
-
-//     prev.addEventListener("click", () => {
-//         if (currsongindex > 0) {
-//             currsongindex--
-//             element = allsongs[currsongindex]
-//             playMusic(element, currsongindex)
-//         }
-//         else {
-//             audio.pause()
-//             play.src = "/Spotify-Clone/controls/play.svg"
-
-//         }
-//     })
-
-//     next.addEventListener("click", () => {
-//         if (currsongindex < allsongs.length - 1) {
-//             currsongindex++
-//             element = allsongs[currsongindex]
-//             playMusic(element, currsongindex)
-//         }
-//         else {
-//             audio.pause()
-//             play.src = "/Spotify-Clone/controls/play.svg"
-//         }
-//     })
-
-//     document.querySelector(".seekbar").addEventListener("click", (e) => {
-//         document.querySelector(".circle").style.width = (e.offsetX / e.target.getBoundingClientRect().width) * 100 + "%"
-//         audio.currentTime = audio.duration * (e.offsetX / e.target.getBoundingClientRect().width)
-//     })
-
-//     playMusic(allsongs[0])
-//     audio.pause()
-//     play.src = "/Spotify-Clone/controls/play.svg"
-
-// }
-// main()
-
-// audio.addEventListener("timeupdate", () => {
-//     document.querySelector(".songtime").innerText =
-//         `${SecToMin(audio.currentTime)} / ${SecToMin(audio.duration)}`
-
-//     document.querySelector(".circle").style.width = (audio.currentTime / audio.duration) * 100 + "%"
-// })
-
-
-// Updated GetSongs function for Vercel
-
-
-
-
-
-
-// async function GetSongs() {
-//     try {
-//         // Create a manifest file approach
-//         // First, try to fetch a songs manifest if it exists
-//         let response = await fetch("./Songs/songs.json");
-        
-//         if (response.ok) {
-//             let data = await response.json();
-//             return data.songs;
-//         }
-        
-//         // Fallback: Try to manually list songs from a known pattern
-//         // You'll need to update this array with your actual song names
-//         console.warn("songs.json not found, using manual list");
-//         return [
-//             "./Songs/Your-Song-1.mp3",
-//             "./Songs/Your-Song-2.mp3",
-//             "./Songs/Your-Song-3.mp3"
-//         ];
-        
-//     } catch (error) {
-//         console.error("Error loading songs:", error);
-//         return [];
-//     }
-// }
-
-
-
 async function GetSongs() {
     try {
         // Try to fetch songs.json from the Songs folder
@@ -217,6 +51,7 @@ async function GetSongs() {
     }
 }
 
+
 function SecToMin(time) {
     if (isNaN(time)) {
         return "00:00"
@@ -240,13 +75,13 @@ function playMusic(element, index) {
     play.src = "/Spotify-Clone/controls/pause.svg"
     currsongindex = index
 
-    // FIXED: Remove URL hardcoding - works for any path
     let song = element
-        .split("/").pop() // Get just the filename
+        .replaceAll("http://127.0.0.1:3000/Spotify-Clone/Songs/", "")
         .replaceAll("%20", " ")
         .replace(".mp3", "")
 
     document.querySelector(".songinfo").innerText = song
+
 }
 
 let audio = new Audio()
@@ -255,13 +90,6 @@ let allsongs = []
 
 let main = async () => {
     let songs = await GetSongs()
-    
-    if (songs.length === 0) {
-        console.error("No songs found!");
-        document.querySelector(".error-message").style.display = "block";
-        return;
-    }
-    
     allsongs = songs
 
     let prev = document.querySelector("#prev")
@@ -270,31 +98,26 @@ let main = async () => {
 
     for (let i = 0; i < songs.length; i++) {
         let element = songs[i];
-        
-        // FIXED: Extract song name without hardcoded paths
-        let fullFileName = element.split("/").pop()
-        let song = fullFileName.replaceAll("%20", " ").replace(".mp3", "")
-        let artist = song.includes("-") ? song.split("-")[1].trim() : "Unknown Artist"
-        let songTitle = song.includes("-") ? song.split("-")[0].trim() : song
+        let song = element.replaceAll("http://127.0.0.1:3000/Spotify-Clone/Songs/", "").replaceAll("%20", " ").replace(".mp3", "")
+        let artist = song.split("-")[1]
 
-        // Create cards
         let card = document.querySelector(".card_container")
         let div = document.createElement("div")
         div.className = "card"
         div.innerHTML = `   <img src="https://i.scdn.co/image/ab67706f000000029e1828b742d18f4756bb6c29" alt="">
-        <h3>${songTitle}</h3>
+        <h3>${song}</h3>
         <p>${artist}</p>`
         card.appendChild(div)
+
 
         div.addEventListener("click", () => {
             playMusic(element, i)
         })
 
-        // Create playlist items
         let middle_card = document.querySelector(".middle")
         let middle_div = document.createElement("div")
         middle_div.className = "sub_box"
-        middle_div.innerHTML = `<div><h5>${songTitle}</h5>
+        middle_div.innerHTML = `<div><h5>${song}</h5>
         <p>${artist}</p></div>
         <img src="play_circle.svg" alt="">`
         middle_card.appendChild(middle_div)
@@ -318,19 +141,20 @@ let main = async () => {
     prev.addEventListener("click", () => {
         if (currsongindex > 0) {
             currsongindex--
-            let element = allsongs[currsongindex]
+            element = allsongs[currsongindex]
             playMusic(element, currsongindex)
         }
         else {
             audio.pause()
             play.src = "/Spotify-Clone/controls/play.svg"
+
         }
     })
 
     next.addEventListener("click", () => {
         if (currsongindex < allsongs.length - 1) {
             currsongindex++
-            let element = allsongs[currsongindex]
+            element = allsongs[currsongindex]
             playMusic(element, currsongindex)
         }
         else {
@@ -340,27 +164,176 @@ let main = async () => {
     })
 
     document.querySelector(".seekbar").addEventListener("click", (e) => {
-        let seekbar = e.target
-        let rect = seekbar.getBoundingClientRect()
-        let percent = (e.clientX - rect.left) / rect.width
-        document.querySelector(".circle").style.width = percent * 100 + "%"
-        audio.currentTime = audio.duration * percent
+        document.querySelector(".circle").style.width = (e.offsetX / e.target.getBoundingClientRect().width) * 100 + "%"
+        audio.currentTime = audio.duration * (e.offsetX / e.target.getBoundingClientRect().width)
     })
 
-    if (allsongs[0]) {
-        playMusic(allsongs[0])
-        audio.pause()
-        play.src = "/Spotify-Clone/controls/play.svg"
-    }
-}
+    playMusic(allsongs[0])
+    audio.pause()
+    play.src = "/Spotify-Clone/controls/play.svg"
 
+}
 main()
 
 audio.addEventListener("timeupdate", () => {
-    if (audio.duration) {
-        document.querySelector(".songtime").innerText =
-            `${SecToMin(audio.currentTime)} / ${SecToMin(audio.duration)}`
+    document.querySelector(".songtime").innerText =
+        `${SecToMin(audio.currentTime)} / ${SecToMin(audio.duration)}`
 
-        document.querySelector(".circle").style.width = (audio.currentTime / audio.duration) * 100 + "%"
-    }
+    document.querySelector(".circle").style.width = (audio.currentTime / audio.duration) * 100 + "%"
 })
+
+
+// Updated GetSongs function for Vercel
+
+
+
+
+
+
+// function SecToMin(time) {
+//     if (isNaN(time)) {
+//         return "00:00"
+//     }
+//     let min = Math.floor(time / 60)
+//     let sec = Math.floor(time % 60)
+
+//     if (sec < 10) {
+//         return `${min}:${"0" + sec}`
+//     }
+//     return `${min}:${sec}`
+// }
+
+// function playMusic(element, index) {
+//     audio.src = element
+//     audio.addEventListener("loadedmetadata", () => {
+//         document.querySelector(".songtime").innerText =
+//             `${SecToMin(audio.currentTime)} / ${SecToMin(audio.duration)}`
+//     }, { once: true })
+//     audio.play()
+//     play.src = "/Spotify-Clone/controls/pause.svg"
+//     currsongindex = index
+
+//     // FIXED: Remove URL hardcoding - works for any path
+//     let song = element
+//         .split("/").pop() // Get just the filename
+//         .replaceAll("%20", " ")
+//         .replace(".mp3", "")
+
+//     document.querySelector(".songinfo").innerText = song
+// }
+
+// let audio = new Audio()
+// let currsongindex = 0
+// let allsongs = []
+
+// let main = async () => {
+//     let songs = await GetSongs()
+    
+//     if (songs.length === 0) {
+//         console.error("No songs found!");
+//         document.querySelector(".error-message").style.display = "block";
+//         return;
+//     }
+    
+//     allsongs = songs
+
+//     let prev = document.querySelector("#prev")
+//     let play = document.querySelector("#play")
+//     let next = document.querySelector("#next")
+
+//     for (let i = 0; i < songs.length; i++) {
+//         let element = songs[i];
+        
+//         // FIXED: Extract song name without hardcoded paths
+//         let fullFileName = element.split("/").pop()
+//         let song = fullFileName.replaceAll("%20", " ").replace(".mp3", "")
+//         let artist = song.includes("-") ? song.split("-")[1].trim() : "Unknown Artist"
+//         let songTitle = song.includes("-") ? song.split("-")[0].trim() : song
+
+//         // Create cards
+//         let card = document.querySelector(".card_container")
+//         let div = document.createElement("div")
+//         div.className = "card"
+//         div.innerHTML = `   <img src="https://i.scdn.co/image/ab67706f000000029e1828b742d18f4756bb6c29" alt="">
+//         <h3>${songTitle}</h3>
+//         <p>${artist}</p>`
+//         card.appendChild(div)
+
+//         div.addEventListener("click", () => {
+//             playMusic(element, i)
+//         })
+
+//         // Create playlist items
+//         let middle_card = document.querySelector(".middle")
+//         let middle_div = document.createElement("div")
+//         middle_div.className = "sub_box"
+//         middle_div.innerHTML = `<div><h5>${songTitle}</h5>
+//         <p>${artist}</p></div>
+//         <img src="play_circle.svg" alt="">`
+//         middle_card.appendChild(middle_div)
+
+//         middle_div.addEventListener("click", () => {
+//             playMusic(element, i)
+//         })
+//     }
+
+//     play.addEventListener("click", () => {
+//         if (audio.paused) {
+//             audio.play()
+//             play.src = "/Spotify-Clone/controls/pause.svg"
+//         }
+//         else {
+//             audio.pause()
+//             play.src = "/Spotify-Clone/controls/play.svg"
+//         }
+//     })
+
+//     prev.addEventListener("click", () => {
+//         if (currsongindex > 0) {
+//             currsongindex--
+//             let element = allsongs[currsongindex]
+//             playMusic(element, currsongindex)
+//         }
+//         else {
+//             audio.pause()
+//             play.src = "/Spotify-Clone/controls/play.svg"
+//         }
+//     })
+
+//     next.addEventListener("click", () => {
+//         if (currsongindex < allsongs.length - 1) {
+//             currsongindex++
+//             let element = allsongs[currsongindex]
+//             playMusic(element, currsongindex)
+//         }
+//         else {
+//             audio.pause()
+//             play.src = "/Spotify-Clone/controls/play.svg"
+//         }
+//     })
+
+//     document.querySelector(".seekbar").addEventListener("click", (e) => {
+//         let seekbar = e.target
+//         let rect = seekbar.getBoundingClientRect()
+//         let percent = (e.clientX - rect.left) / rect.width
+//         document.querySelector(".circle").style.width = percent * 100 + "%"
+//         audio.currentTime = audio.duration * percent
+//     })
+
+//     if (allsongs[0]) {
+//         playMusic(allsongs[0])
+//         audio.pause()
+//         play.src = "/Spotify-Clone/controls/play.svg"
+//     }
+// }
+
+// main()
+
+// audio.addEventListener("timeupdate", () => {
+//     if (audio.duration) {
+//         document.querySelector(".songtime").innerText =
+//             `${SecToMin(audio.currentTime)} / ${SecToMin(audio.duration)}`
+
+//         document.querySelector(".circle").style.width = (audio.currentTime / audio.duration) * 100 + "%"
+//     }
+// })
